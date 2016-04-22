06871bb4bf04c6dc31dc9621af6f69ee2140734e
yglin <yglin.mlanser@gmail.com>
2016-04-22 15:04:44 +0800

    Fix bug of 456route.originalPath is not an absolute url

--- client/components/auth/router.decorator.js
+++ client/components/auth/router.decorator.js
@@ -3,49 +3,63 @@
 (function() {
 
 angular.module('spirit99StationApp.auth')
-    .run(function($rootScope, $window, $cookies, $location, Auth) {
-        // Redirect to login if route requires auth and the user is not logged in, or doesn't have required role
-        $rootScope.$on('$routeChangeStart', function(event, next, current) {
-            // console.log(current);
-            if (!next.authenticate) {
-                return;
-            }
+.run(function($rootScope, $window, $cookies, $location, Auth) {
+    // Redirect to login if route requires auth and the user is not logged in, or doesn't have required role
+    $rootScope.$on('$routeChangeStart', function(event, next, current) {
+        // console.log(current);
+        if (!next.authenticate) {
+            return;
+        }
 
-            if (typeof next.authenticate === 'string') {
-                Auth.hasRole(next.authenticate, _.noop).then(has => {
-                    if (has) {
-                        return;
-                    }
+        if (typeof next.authenticate === 'string') {
+            Auth.hasRole(next.authenticate, _.noop).then(has => {
+                if (has) {
+                    return;
+                }
 
-                    return Auth.isLoggedIn(_.noop).then(is => {
-                        if (is) {
-                            $window.alert('抱歉，您沒有權限執行此動作');
-                            if (current && current.$$route && current.$$route.originalPath) {
-                                $location.path(current.$$route.originalPath);
-                            }
-                            else {
-                                $location.path('/');
-                            }
+                return Auth.isLoggedIn(_.noop).then(is => {
+                    if (is) {
+                        $window.alert('抱歉，您沒有權限執行此動作');
+                        if (current && current.$$route && current.$$route.originalPath) {
+                            $location.path(buildPathFromRoute(current));
                         }
                         else {
-                            if (next && next.$$route && next.$$route.originalPath) {
-                                $cookies.put('path-after-login', next.$$route.originalPath);
-                            }
-                            $location.path('/login');
+                            $location.path('/');
                         }
-                    });
-                });
-            } else {
-                Auth.isLoggedIn(_.noop).then(is => {
-                    if (is) {
-                        return;
                     }
-
-                    event.preventDefault();
-                    $location.path('/');
+                    else {
+                        if (next && next.$$route && next.$$route.originalPath) {
+                            $cookies.put('path-after-login', buildPathFromRoute(next));
+                        }
+                        $location.path('/login');
+                    }
                 });
-            }
-        });
+            });
+        } else {
+            Auth.isLoggedIn(_.noop).then(is => {
+                if (is) {
+                    return;
+                }
+
+                event.preventDefault();
+                $location.path('/');
+            });
+        }
     });
+
+    function buildPathFromRoute(routeObj)
+    {
+        var path = routeObj.$$route.originalPath;
+        for (var property in routeObj.pathParams)
+        {
+            if (routeObj.pathParams.hasOwnProperty(property))
+            {
+                var regEx = new RegExp(":" + property, "gi");
+                path = path.replace(regEx, routeObj.pathParams[property].toString());
+            }
+        }
+        return path;
+    }
+});
 
 })();
