5a94f6a610dfc8229406a922b666f4b4fe59bb6b
yglin <yglin.mlanser@gmail.com>
2016-05-31 14:15:24 +0800

    Post View: show edit and delete button if is my post

--- client/app/post/view.controller.js
+++ client/app/post/view.controller.js
@@ -2,7 +2,7 @@
 * @Author: yglin
 * @Date:   2016-05-02 09:21:37
 * @Last Modified by:   yglin
-* @Last Modified time: 2016-05-05 17:15:05
+* @Last Modified time: 2016-05-31 13:41:12
 */
 
 'use strict';
@@ -14,10 +14,10 @@
         .module('spirit99StationApp.post')
         .controller('PostViewController', PostViewController);
 
-    PostViewController.$inject = ['$window', '$routeParams', 'Util', 'Channel', 'Post'];
+    PostViewController.$inject = ['$window', '$routeParams', 'Util', 'Channel', 'Post', 'Auth'];
 
     /* @ngInject */
-    function PostViewController($window, $routeParams, Util, Channel, Post) {
+    function PostViewController($window, $routeParams, Util, Channel, Post, Auth) {
         var $ctrl = this;
         $ctrl.title = 'Post View';
         $ctrl.channel = undefined;
@@ -34,6 +34,8 @@
                 Post.get($routeParams.channel_id, $routeParams.post_id)
                 .then(function (post) {
                     $ctrl.post = post;
+
+                    // Get channel data
                     Channel.get($routeParams.channel_id).then(function (channel) {
                         $ctrl.channel = channel;
                         if (post.category && channel.categories
@@ -43,6 +45,17 @@
                             $ctrl.categoryTitle = category.title;
                         }
                     });
+
+                    // Check if my post
+                    Auth.getCurrentUser(function (user) {
+                        if (user && user._id && post.owner_id == user._id) {
+                            $ctrl.isMyPost = true;
+                        }
+                        else {
+                            $ctrl.isMyPost = false;
+                        }
+                    });
+                    
                 }, function (error) {
                     $window.alert('找不到文章: channel id = ' + $routeParams.channel_id + ', post id = ' + $routeParams.post_id);
                     Util.returnUrl();
