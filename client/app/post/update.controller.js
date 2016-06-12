49253b867e5d9e4b26584c3a96f0079fb7a3a461
yglin <yglin.mlanser@gmail.com>
2016-06-12 15:15:06 +0800

    Handle logout event in post update page

--- client/app/post/update.controller.js
+++ client/app/post/update.controller.js
@@ -2,7 +2,7 @@
 * @Author: yglin
 * @Date:   2016-05-03 19:53:57
 * @Last Modified by:   yglin
-* @Last Modified time: 2016-06-03 13:40:25
+* @Last Modified time: 2016-06-12 15:11:01
 */
 
 'use strict';
@@ -14,10 +14,10 @@
         .module('spirit99StationApp.post')
         .controller('PostUpdateController', PostUpdateController);
 
-    PostUpdateController.$inject = ['$location', 'Util', '$routeParams', 'Auth', 'Channel', 'Post', 'ygDialog'];
+    PostUpdateController.$inject = ['$scope', '$location', 'Util', '$routeParams', 'Auth', 'Channel', 'Post', 'ygDialog'];
 
     /* @ngInject */
-    function PostUpdateController($location, Util, $routeParams, Auth, Channel, Post, ygDialog) {
+    function PostUpdateController($scope, $location, Util, $routeParams, Auth, Channel, Post, ygDialog) {
         var $ctrl = this;
         $ctrl.title = 'Post Update View';
         $ctrl.channel = undefined;
@@ -32,7 +32,7 @@
             if (!$routeParams.channel_id || !$routeParams.post_id) {
                 ygDialog.alert('網址錯誤', '<h3>網址中未指定頻道ID或文章ID</h3><br><p>' + $location.absUrl() + '</p>')
                 .then(function () {
-                    Util.returnUrl();                
+                    Util.returnUrl('/');                
                 });
                 return;
             }
@@ -50,17 +50,21 @@
                             console.error(error);
                             ygDialog.alert('找不到文章', '<h3>找不到文章，或是你沒有權限修改，錯誤回應：</h3><br><p>' + JSON.stringify(error) + '</p>')
                             .then(function () {
-                                Util.returnUrl();
+                                Util.returnUrl('/');
                             });
                         });
                     }, function (error) {
                         console.error(error);
                         ygDialog.alert('找不到頻道', '<h3>找不到頻道 ' + $routeParams.channel_id + ' 的資料，錯誤回應：</h3><br><p>' + JSON.stringify(error) + '</p>')
                         .then(function () {
-                            Util.returnUrl();
+                            Util.returnUrl('/');
                         });
                     });
                 }
+            });
+
+            $scope.$on('account:logout', function () {
+                Util.returnUrl('/'); 
             });
         }
 
