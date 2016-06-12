4530ac99b866f5dd968296dbd0f2ba2bd93b157d
yglin <yglin.mlanser@gmail.com>
2016-06-12 11:36:48 +0800

    Profile page layout

--- client/app/account/profile/profile.controller.js
+++ client/app/account/profile/profile.controller.js
@@ -2,7 +2,7 @@
 * @Author: yglin
 * @Date:   2016-06-09 09:58:46
 * @Last Modified by:   yglin
-* @Last Modified time: 2016-06-09 10:11:31
+* @Last Modified time: 2016-06-12 11:30:36
 */
 
 (function() {
@@ -12,10 +12,10 @@
         .module('spirit99StationApp')
         .controller('ProfileController', ProfileController);
 
-    ProfileController.$inject = ['$scope', 'Util', 'Channel', 'Post'];
+    ProfileController.$inject = ['$scope', 'Util', 'Auth', 'Channel', 'Post'];
 
     /* @ngInject */
-    function ProfileController($scope, Util, Channel, Post) {
+    function ProfileController($scope, Util, Auth, Channel, Post) {
         var $ctrl = this;
         $ctrl.title = 'Profile';
         $ctrl.user = undefined;
@@ -27,6 +27,20 @@
         function activate() {
             $scope.$on('account:logout', function () {
                 Util.returnUrl(); 
+            });
+
+            Auth.getCurrentUser(function (user) {
+                if (user._id) {
+                    $ctrl.user = user;
+                }
+            });
+
+            $scope.$on('account:login', function () {
+                Auth.getCurrentUser(function (user) {
+                    if (user._id) {
+                        $ctrl.user = user;
+                    }
+                });                
             });
         }
     }
