af40c158727532d174c70b7a7f3d19a2de9cd74f
yglin <yglin.mlanser@gmail.com>
2016-07-05 17:41:22 +0800

    Add class s99-ng-clickable class to channel-item and post-item

--- client/app/channel/channel-item.component.js
+++ client/app/channel/channel-item.component.js
@@ -2,7 +2,7 @@
 * @Author: yglin
 * @Date:   2016-04-12 15:09:47
 * @Last Modified by:   yglin
-* @Last Modified time: 2016-07-05 13:41:30
+* @Last Modified time: 2016-07-05 17:26:06
 */
 
 'use strict';
@@ -15,23 +15,17 @@
         templateUrl: 'app/channel/channel-item.tpl.html',
         controller: ChannelItemController,
         bindings: {
-            channel: '='
+            channel: '=',
+            onClick: '&'
         }
     });
 
-    ChannelItemController.$inject = ['appConfig', '$window', 'ygDialog', 'Channel'];
+    ChannelItemController.$inject = [];
 
     /* @ngInject */
-    function ChannelItemController(appConfig, $window, ygDialog, Channel) {
+    function ChannelItemController() {
         var $ctrl = this;
         $ctrl.title = 'ChannelItem';
-        $ctrl.import = _import;
-
-        function _import() {
-            ygDialog.confirm('匯入頻道', '匯入頻道至' + appConfig.spirit99Url + '?')
-            .then(function () {
-                $window.open(Channel.getImportUrl($ctrl.channel.id), 'spirit99').focus();
-            });
-        }
+        console.log($ctrl.onClick);
     }
 })();
--- client/app/channel/channel-list.controller.js
+++ client/app/channel/channel-list.controller.js
@@ -2,7 +2,7 @@
 * @Author: yglin
 * @Date:   2016-04-22 10:51:05
 * @Last Modified by:   yglin
-* @Last Modified time: 2016-06-08 16:38:10
+* @Last Modified time: 2016-07-05 17:19:16
 */
 
 'use strict';
@@ -14,14 +14,15 @@
         .module('spirit99StationApp.channel')
         .controller('ChannelListController', ChannelListController);
 
-    ChannelListController.$inject = ['$location', '$http', 'Channel'];
+    ChannelListController.$inject = ['appConfig', '$window', '$location', '$http', 'Channel', 'ygDialog'];
 
     /* @ngInject */
-    function ChannelListController($location, $http, Channel) {
+    function ChannelListController(appConfig, $window, $location, $http, Channel, ygDialog) {
         var $ctrl = this;
         $ctrl.title = 'Channel List';
         $ctrl.channels = [];
         $ctrl.gotoCreatePage = gotoCreatePage;
+        $ctrl.import = _import;
 
         activate();
 
@@ -34,6 +35,13 @@
 
         function gotoCreatePage () {
             $location.path('/channels/create');
+        }
+
+        function _import(channel_id) {
+            ygDialog.confirm('匯入頻道', '匯入頻道至' + appConfig.spirit99Url + '?')
+            .then(function () {
+                $window.open(Channel.getImportUrl(channel_id), 'spirit99').focus();
+            });
         }
 
     }
